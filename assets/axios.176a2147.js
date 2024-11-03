6f8964e8dadf29e97b726b38475ef6b201a3ac44
AkshayBhanawala <AkshayBhanawala@Gmail.com>
2024-11-03 23:23:10 +0000

    Deploying to gh-pages from @ AkshayBhanawala/ProTrack-TaskDashboard@8e6d008281403e9d53d19c7d26a0687b8d2627f0 🚀

--- assets/axios.176a2147.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./axios.0ed95acd.js";import{j as i}from"./index.68b1c18c.js";const e=o.create({baseURL:"https://api.example.com"});var t=i(({app:a})=>{a.provide("axios",o),a.provide("api",e)});export{e as api,t as default};
