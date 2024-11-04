5f238c4f4bedf9276aa61ec0d96ee6f191dfdcd9
AkshayBhanawala <AkshayBhanawala@Gmail.com>
2024-11-04 06:46:28 +0000

    Deploying to gh-pages from @ AkshayBhanawala/ProTrack-TaskDashboard@624b4f01cd7524a06ba996e3cdda214009cdfc2e 🚀

--- assets/apex-charts.6f13411c.js
+++ assets/apex-charts.6f13411c.js
@@ -1,4 +1,4 @@
-import{d as si,r as yt,k as ri,o as ni,g as oi,l as li,t as hi,w as De,h as ci,n as di,j as gi}from"./index.1d11aa59.js";var at={exports:{}};/*!
+import{d as si,r as yt,o as ri,a as ni,g as oi,b as li,t as hi,w as De,h as ci,n as di,c as gi}from"./index.4b21b7cf.js";var at={exports:{}};/*!
  * ApexCharts v3.54.1
  * (c) 2018-2024 ApexCharts
  * Released under the MIT License.
--- assets/axios.5d428d25.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./axios.0ed95acd.js";import{j as i}from"./index.1d11aa59.js";const e=o.create({baseURL:"https://api.example.com"});var t=i(({app:a})=>{a.provide("axios",o),a.provide("api",e)});export{e as api,t as default};
